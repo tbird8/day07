2d212c868d8fec0a0922662b4b5fea994732b3b8
tee-jay bird <tbird@c2r2s5.wethinkcode.co.za>
2020-06-26 14:24:22 +0200

    .

---
+++ ex02/ft_ultimate_range.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_range.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbird <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/06/26 13:57:38 by tbird             #+#    #+#             */
+/*   Updated: 2020/06/26 14:23:46 by tbird            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int ft_ultimate_range(int **range, int min, int max)
+
+{
+    int i; 
+	int 0;
+	int *lol;
+	 
+	if (min>=max)
+	{
+		*range = (void )*0;
+		return (0);
+	
+	tab = (int*)malloc(sizeof(*tab);
+			}
+			{
+			while	( (min -max))
+		    lol[i] = max;
+			i++;
+			min++;
+			}
+			return(0);
+			
+	
+}
